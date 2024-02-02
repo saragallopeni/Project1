9ea1356a4edf75b239d9eee8fd981588fa7d03c5
saragallopeni <saragallopeni44@gmail.com>
2024-02-02 18:55:38 +0100

    infinite scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FlickrServiceService } from './service/flickr-service.service';
+
+export interface Photo {
+  title: string;
+  id: string;
+  media: {
+    m: string;
+  };
+  author: {
+    name: string;
+    uri: string;
+  };
+  published: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'flickrApi';
-  
-  photos: any[] = [];
-  
+  photos: Photo[] = [];
+  page: number = 1;
+  currentKeyword: string = '';  
 
   constructor(private flickrService: FlickrServiceService) {}
-  
-
 
   performSearch(keyword: string): void {
-    this.flickrService.searchPhotos(keyword).subscribe((data: any) => {
-        if (data && data.items) {
-          this.photos = data.items; 
-        }
-      });
-    }
+    this.currentKeyword = keyword;
+    this.flickrService.searchPhotos(keyword, this.page).subscribe((data: any) => {
+      console.log('Te dhenat', data);
+      if (data && data.items) {
+        this.photos = this.photos.concat(data.items as Photo[]); 
+        this.page++;
+      }
+    });
+  }
 
+ @HostListener('window:scroll')
+onScroll() {
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const bodyHeight = document.body.offsetHeight;
+
+  if (scrollPosition >= bodyHeight - 500) {
+    this.performSearch(this.currentKeyword); 
+  }
+}
 
 }
-     
-
-  
-  
--- src/app/service/flickr-service.service.ts
+++ src/app/service/flickr-service.service.ts
@@ -7,13 +7,14 @@ import { Observable, catchError, tap, throwError } from 'rxjs';
 })
 export class FlickrServiceService {
 
+
   private apiKey = 'dd6df9c0a2c31cd8ecd88834d4846b9b';
   private apiUrl = 'https://cors-anywhere.herokuapp.com/https://www.flickr.com/services/feeds/photos_public.gne';
 
   constructor(private http: HttpClient) { }
 
-  searchPhotos(tags: string): Observable<any> {
-    const url = `${this.apiUrl}?tags=${tags}&format=json&nojsoncallback=JSONP_CALLBACK`;
+  searchPhotos(tags: string, page: number): Observable<any> {
+    const url = `${this.apiUrl}?tags=${tags}&format=json&nojsoncallback=JSONP_CALLBACK&api_key=${this.apiKey}`;
     return this.http.get(url);
   }
 
